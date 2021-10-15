73218d62d5a6e38fff6066f91b90ea56e1eafe8c
cabins <kong_lingcun@hotmail.com>
2021-10-15 23:39:12 +0800

    use default font size

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -17,18 +17,21 @@
 (defun tenon/setup-font ()
   "Font setup."
 
+  (interactive)
   (let* ((enfonts '("Cascadia Code" "Source Code Pro" "Courier New" "Monaco"))
 	 (cnfonts '("STKaiti" "华文楷体" "STHeiti" "华文黑体" "微软雅黑"))
 	 (cnfont (font-available cnfonts))
 	 (enfont (font-available enfonts)))
     (when enfont
       (set-face-attribute
-       'default nil :font (format "%s-%d" enfont 9.5)))
+       'default nil :font (format "%s" enfont)))
     (when cnfont
       (dolist (charset '(kana han cjk-misc bopomofo))
 	(set-fontset-font t charset cnfont))
       (setq face-font-rescale-alist
-	    (mapcar (lambda (item) (cons item 1.2)) cnfonts)))))
+	    (mapcar (lambda (item)
+		      (cons item 1.2))
+		    cnfonts)))))
 
 (add-hook 'after-init-hook 'tenon/setup-font)
 (add-hook 'after-init-hook 'tenon/cleaner-gui)
